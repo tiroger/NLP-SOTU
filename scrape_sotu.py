9feb7f1d5247c2d6fcb8df8ca39f0a284579ae08
Roger Lefort <tirogerl@gmail.com>
2020-03-21 20:01:06 -0400

    Fixed parser and added dates and presidents list

--- scrape_sotu.py
+++ scrape_sotu.py
@@ -7,9 +7,13 @@ from selenium.common.exceptions import TimeoutException
 
 import pickle
 
-sotu_raw_transcripts = []
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-dates = list(range(1793, 1797)) # Dates 1793-2020
+sotu_raw_transcripts = []
+presidents = []
+dates = []
+
+dates = list(range(1793, 2021)) # Dates 1793-2020
 dates_str = [str(d) for d in dates]
 dates_str
 
@@ -18,21 +22,25 @@ for date in dates_str:
     try:
         # Opening URL
         base_url = 'https://www.presidency.ucsb.edu/documents/presidential-documents-archive-guidebook/annual-messages-congress-the-state-the-union'
-        driver = webdriver.Chrome('./chromedriver')
+        driver = webdriver.Chrome()
         driver.get(base_url)
         driver.implicitly_wait(10) # seconds
         element = driver.find_element_by_link_text(date)
         webdriver.ActionChains(driver).move_to_element(element).click(element).perform()
         current_url = driver.current_url # url containing transcript of SOTU addresses
-        #print(current_url)
-        r = requests.get(current_url)
-        #print(r.content)
-        soup = BeautifulSoup(r.content, 'html5lib') 
+        r = requests.get(current_url, headers=headers)
+        print(r)
+        # print(r.content)
+        soup = BeautifulSoup(r.content, 'html.parser')
         #print(soup.prettify())
         # Using BeautifulSoup to extract transcript
-        text = soup.find('div', class_='field-docs-content').text
-        text = soup.find('div', class_='node-documents').text
+        text = soup.find('div', class_='field-docs-content').get_text()
+        #text = soup.find('div', class_='node-documents').get_text()
+        pres_name = soup.find('h3', class_='diet-title').get_text()
+        date = soup.find('span', class_='date-display-single').get_text()
         sotu_raw_transcripts.append(text)
+        presidents.append(pres_name)
+        dates.append(date)
         driver.close()
     except:
         print(f'Could not get record for {date}')
@@ -44,3 +52,11 @@ driver.quit()
 # Pickling raw transcripts
 with open('./pickled_files/sotu_raw_transcripts.pkl', 'wb') as f:
     pickle.dump(sotu_raw_transcripts, f)
+
+# Pickling list of presidents
+with open('./pickled_files/presidents.pkl', 'wb') as f:
+    pickle.dump(presidents, f)
+
+# Opening picled list of raw transcripts
+with open('./pickled_files/sotu_raw_transcripts.pkl', 'rb') as f:
+    sotu_raw_transcripts = pickle.load(f)
